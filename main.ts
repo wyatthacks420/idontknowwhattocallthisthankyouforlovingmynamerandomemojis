584e6823c8f00dc0267ef3e114b9b8b36ce19314
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-04-12 11:29:37 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onButtonPressed(Button.A, function () {
+    strip.rotate(1)
+    strip.show()
+    basic.pause(100)
+    basic.pause(500)
+    wuKong.setAllMotor(-100, 100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(100, 100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(100, -100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.pause(500)
+    wuKong.setAllMotor(-100, -100)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+let strip: neopixel.Strip = null
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    strip.showRainbow(1, 360)
+})
